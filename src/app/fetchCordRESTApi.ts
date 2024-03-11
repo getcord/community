e7628e0659758c9777fb70d800036f8048abeb70
khadija <khadija@cord.com>
2024-03-11 15:06:28 +0000

    Create util function to fetch from cord

---
+++ src/app/fetchCordRESTApi.ts
@@ -0,0 +1,27 @@
+import { getServerAuthToken } from "@cord-sdk/server";
+import { CORD_APP_ID, CORD_SECRET, CORD_API_URL } from "../consts";
+
+export async function fetchCordRESTApi<T>(
+  endpoint: string,
+  method: "GET" | "PUT" | "POST" | "DELETE" = "GET",
+  body?: string
+): Promise<T> {
+  const serverAuthToken = getServerAuthToken(CORD_APP_ID, CORD_SECRET);
+  const response = await fetch(`${CORD_API_URL}${endpoint}`, {
+    method,
+    body,
+    headers: {
+      Authorization: `Bearer ${serverAuthToken}`,
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (response.ok) {
+    return response.json();
+  } else {
+    const responseText = await response.text();
+    throw new Error(
+      `Error making Cord API call: ${response.status} ${response.statusText} ${responseText}`
+    );
+  }
+}
