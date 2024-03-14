a81b268bd8e1d0ebf9ea9512d649769c6b007d87
Maylynn <maylynn@cord.com>
2024-03-14 14:06:51 +0000

    Type fix: params in Content should be optional

--- src/app/category/page.tsx
+++ src/app/category/page.tsx
@@ -1,6 +1,6 @@
 import ThreadList from "../components/ThreadList";
 
-export default function Content({ params }: { params: { category: string } }) {
+export default function Content({ params }: { params?: { category: string } }) {
   const category = params ? params.category : 'all'
   return (
       <ThreadList category={category} />
