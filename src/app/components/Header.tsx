ac6bae75bdc599518dff0987c3e804211a5b24b3
khadija <khadija@cord.com>
2024-03-14 10:32:39 +0000

    Add sign in button and page

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,7 +1,19 @@
-import styles from './header.module.css';
+import Link from "next/link";
+import styles from "./header.module.css";
+import { CORD_USER_COOKIE } from "@/consts";
+import { cookies } from "next/headers";
 
 export default function Header() {
-  return <div className={styles.container}>
-    CORD COMMUNITY
-  </div>
+  const userIdCookie = cookies().get(CORD_USER_COOKIE);
+
+  return (
+    <div className={styles.container}>
+      CORD COMMUNITY
+      {!userIdCookie && (
+        <Link href="/signin" className={styles.signinButton}>
+          Sign in to comment
+        </Link>
+      )}
+    </div>
+  );
 }
