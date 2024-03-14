7b65c90d021c4fd3d0e2372dbfd651ca6f2729a7
khadija <khadija@cord.com>
2024-03-14 10:27:14 +0000

    Create a header that we'll show on top of each page
    This will be used to displat filters and the 'add a comment' button

---
+++ src/app/category/[category]/ThreadHeader.tsx
@@ -0,0 +1,29 @@
+import styles from "./threadheader.module.css";
+import cx from "classnames";
+
+export default async function ThreadHeader({
+  permissions,
+}: {
+  permissions: string;
+}) {
+  console.log({ permissions });
+  return (
+    <div className={cx(styles.container)}>
+      <div className={styles.filters}>
+        {/* 
+         TODO: fetch all tags and put them in a dropdown menu button
+         */}
+        FILTERS WOULD APPEAR HERE
+      </div>
+
+      {permissions !== "NOT_VISIBLE" && (
+        <button
+          className={styles.actionButton}
+          disabled={permissions === "READ"}
+        >
+          + Start a discussion
+        </button>
+      )}
+    </div>
+  );
+}
