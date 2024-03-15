efdbe37fc02efe9b3cb190affe01f010198d42e8
Maylynn <maylynn@cord.com>
2024-03-15 17:02:23 +0000

    feat: add cord logo to header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,13 +2,15 @@ import Link from 'next/link';
 import styles from './header.module.css';
 import { CORD_USER_COOKIE } from '@/consts';
 import { cookies } from 'next/headers';
+import logo from '@/app/static/cord-logo.png';
+import Image from 'next/image';
 
 export default function Header() {
   const userIdCookie = cookies().get(CORD_USER_COOKIE);
 
   return (
     <div className={styles.container}>
-      CORD COMMUNITY
+      <Image alt="cord logo" src={logo} height={40}/>
       {!userIdCookie && (
         <Link href="/signin" className={styles.signinButton}>
           Sign in to comment
