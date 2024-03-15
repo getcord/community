fec84ebbca6568506f4ff927ed3fb82780df4d8e
khadija <khadija@cord.com>
2024-03-15 14:20:00 +0000

    Add .prettierrc

--- src/Colors.ts
+++ src/Colors.ts
@@ -1,38 +1,38 @@
-const GREY_X_DARK = "#191A1E";
-const PURPLE = "#9A6AFF";
-const OPACITY_33 = "54";
+const GREY_X_DARK = '#191A1E';
+const PURPLE = '#9A6AFF';
+const OPACITY_33 = '54';
 
 // Please use the full 6-character color syntax, otherwise weird things happen
 // when you try to append opacity modifiers, for example the TRANSLUCENT colors
 export const Colors = {
-  ALERT_LIGHT: "#FDEFEF",
-  ALERT: "#EB5757",
-  GREEN: "#71BC8F",
-  GREY_X_LIGHT: "#F6F6F6",
-  GREY_LIGHT: "#DADCE0",
-  GREY: "#AAAAAC",
-  GREY_DARK: "#76767A",
+  ALERT_LIGHT: '#FDEFEF',
+  ALERT: '#EB5757',
+  GREEN: '#71BC8F',
+  GREY_X_LIGHT: '#F6F6F6',
+  GREY_LIGHT: '#DADCE0',
+  GREY: '#AAAAAC',
+  GREY_DARK: '#76767A',
   GREY_X_DARK,
-  ORANGE_LIGHT: "#FBE3D6",
-  ORANGE: "#F88D76",
+  ORANGE_LIGHT: '#FBE3D6',
+  ORANGE: '#F88D76',
   PURPLE,
-  PURPLE_LIGHT: "#D1D0F9",
-  PURPLE_DARK: "#5F5EB3",
-  BRAND_PURPLE_LIGHT: "#F6F1FF",
-  BRAND_PURPLE_DARK: "#9A6AFF",
-  BRAND_PURPLE_DARKER: "#6949AC",
-  ACID_YELLOW: "#F4FFA0",
-  YELLOW: "#F2FFA1",
-  BLACK: "#000000",
-  WHITE: "#FFFFFF",
-  FOCUS: "#CAE3F1",
+  PURPLE_LIGHT: '#D1D0F9',
+  PURPLE_DARK: '#5F5EB3',
+  BRAND_PURPLE_LIGHT: '#F6F1FF',
+  BRAND_PURPLE_DARK: '#9A6AFF',
+  BRAND_PURPLE_DARKER: '#6949AC',
+  ACID_YELLOW: '#F4FFA0',
+  YELLOW: '#F2FFA1',
+  BLACK: '#000000',
+  WHITE: '#FFFFFF',
+  FOCUS: '#CAE3F1',
   TRANSLUCENT_DARK: GREY_X_DARK + OPACITY_33,
   TRANSLUCENT_PURPLE: PURPLE + OPACITY_33,
-  TRANSPARENT: "transparent",
-  INHERIT: "inherit",
-  BRAND_PRIMARY: "#121314",
-  CONTENT_PRIMARY: "#696A6C",
-  CONTENT_SECONDARY: "#97979F",
+  TRANSPARENT: 'transparent',
+  INHERIT: 'inherit',
+  BRAND_PRIMARY: '#121314',
+  CONTENT_PRIMARY: '#696A6C',
+  CONTENT_SECONDARY: '#97979F',
 };
 
 export type Color = keyof typeof Colors;
@@ -42,7 +42,7 @@ export function isCordColor(color: string): color is Color {
 }
 
 export function withOpacityZero(color: Color) {
-  return Colors[color] + "00";
+  return Colors[color] + '00';
 }
 
 /*
@@ -53,7 +53,7 @@ export function withOpacityZero(color: Color) {
 */
 export function cssColorFromColorArgument(
   color: Color | string | undefined,
-  defaultColor: Color
+  defaultColor: Color,
 ) {
   // if nothing is provided, return the default
   if (!color) {
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,21 @@
-import type { Metadata } from "next";
-import "./global.css";
-import { getClientAuthToken } from "@cord-sdk/server";
-import CordIntegration from "./CordIntegration";
-import Header from "./components/Header";
-import Sidebar from "./components/Sidebar";
-import styles from "./styles.module.css";
+import type { Metadata } from 'next';
+import './global.css';
+import { getClientAuthToken } from '@cord-sdk/server';
+import CordIntegration from './CordIntegration';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import styles from './styles.module.css';
 
 async function getData() {
   const { CORD_SECRET, CORD_APP_ID } = process.env;
   if (!CORD_SECRET || !CORD_APP_ID) {
     console.error(
-      "Missing CORD_SECRET or CORD_APP_ID env variable. Get it on console.cord.com and add it to .env"
+      'Missing CORD_SECRET or CORD_APP_ID env variable. Get it on console.cord.com and add it to .env',
     );
     return { clientAuthToken: null };
   }
   const clientAuthToken = getClientAuthToken(CORD_APP_ID, CORD_SECRET, {
-    user_id: "khadija",
+    user_id: 'khadija',
   });
   return {
     clientAuthToken,
@@ -23,8 +23,8 @@ async function getData() {
 }
 
 export const metadata: Metadata = {
-  title: "Cord Community",
-  description: "Hub for Cord users to connect",
+  title: 'Cord Community',
+  description: 'Hub for Cord users to connect',
 };
 
 export default async function RootLayout({
