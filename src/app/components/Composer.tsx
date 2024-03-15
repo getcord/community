f79aa1eca4a5f0867409e650d136c4f4b11bee33
khadija <khadija@cord.com>
2024-03-15 12:46:54 +0000

    replace composer, send button and composer layout

--- src/app/components/Composer.tsx
+++ src/app/components/Composer.tsx
@@ -1,9 +1,74 @@
 "use client";
 
-import { useState } from "react";
+import { useCallback, useState } from "react";
+import type { ClientMessageData } from "@cord-sdk/types";
 import { experimental } from "@cord-sdk/react";
 import styles from "./composer.module.css";
 import CategorySelector from "@/app/components/CategorySelector";
+import {
+  ComposerLayoutProps,
+  ComposerProps,
+  SendButtonProps,
+} from "@cord-sdk/react/dist/mjs/types/experimental";
+
+function DatCordComposer(props: ComposerProps) {
+  const [title, setTitle] = useState("");
+  const datCordOnSubmit = useCallback(
+    ({ message }: { message: Partial<ClientMessageData> }) => {
+      if (!title || !message) {
+        return;
+      }
+
+      props.onSubmit({ message });
+    },
+    [title, props]
+  );
+
+  return (
+    <experimental.CordComposer
+      {...props}
+      onSubmit={(message) => {
+        datCordOnSubmit(message);
+      }}
+      onKeyDown={({ event }: { event: React.KeyboardEvent }) => {
+        if (event.key === "Enter") {
+          return;
+        }
+        props.onKeyDown({ event });
+      }}
+      onResetState={() => {
+        if (!title) {
+          return;
+        }
+        setTitle("");
+        props.onResetState();
+      }}
+    />
+  );
+}
+
+function CommunityComposerLayout(props: ComposerLayoutProps) {
+  const sendButton = props.toolbarItems?.find(
+    (item) => item.name === "sendButton"
+  );
+
+  return (
+    <div>
+      <experimental.ComposerLayout
+        toolbarItems={props.toolbarItems?.filter(
+          (item) => item.name !== "sendButton"
+        )}
+        textEditor={props.textEditor}
+      ></experimental.ComposerLayout>
+
+      {sendButton?.element}
+    </div>
+  );
+}
+
+function DatCordSendButton(props: SendButtonProps) {
+  return <button onClick={props.onClick}>SEND BUTTON</button>;
+}
 
 export default function Composer() {
   const [title, setTitle] = useState("");
@@ -37,7 +102,13 @@ export default function Composer() {
         <label htmlFor="titleInput" className={styles.label}>
           Message:
         </label>
-        <experimental.Replace>
+        <experimental.Replace
+          replace={{
+            ComposerLayout: CommunityComposerLayout,
+            Composer: DatCordComposer,
+            SendButton: DatCordSendButton,
+          }}
+        >
           <experimental.SendComposer
             createThread={{
               groupID: "samplecord",
