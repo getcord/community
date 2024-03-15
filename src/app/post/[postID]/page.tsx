add26fc993ef8a1c14c6deabe9d1b179a617793e
Maylynn <maylynn@cord.com>
2024-03-15 16:14:20 +0000

    feat: update post page
    adds a very basic non styled cord composer, adds the category & whether the post is admin made or pinned

--- src/app/post/[postID]/page.tsx
+++ src/app/post/[postID]/page.tsx
@@ -1,19 +1,39 @@
 'use client';
 
-import { Message, thread as threadHooks } from '@cord-sdk/react';
+import { Composer, Message, thread as threadHooks } from '@cord-sdk/react';
+import styles from './post.module.css';
+import { getTypedMetadata, mapCategoryEndpointsToTitles } from '@/utils';
+import { NavButton } from '@/app/components/NavButton';
+import { LockClosedIcon } from '@heroicons/react/24/outline';
+import { PushPinSvg } from '@/app/components/PushPinSVG';
 
 export default function Post({ params }: { params?: { postID: string } }) {
   const threadID = decodeURIComponent(params?.postID || '');
   const thread = threadHooks.useThread(threadID);
 
-  if (!params?.postID) {
+  if (!params?.postID || !thread) {
     return <p>oops we couldn&apos;t find that post - sorry!</p>;
   }
 
+  const metadata = getTypedMetadata(thread.thread?.metadata || {});
+
   return (
-    <div>
-      <h1>{thread.thread?.name}</h1>
+    <div className={styles.container}>
+      <div className={styles.heading}>
+        <span className={styles.icons}>
+          {metadata.admin && <LockClosedIcon width="24px" strokeWidth={3} />}
+          {metadata.pinned && <PushPinSvg className={`${styles.pinIcon}`} />}
+        </span>
+        <h1 className={styles.threadName}>{thread.thread?.name}</h1>
+      </div>
+      <NavButton
+        type="category"
+        value={mapCategoryEndpointsToTitles(metadata.category || '')}
+        linkTo={`/${metadata.category}`}
+        isActive={false}
+      />
       <Message threadId={threadID} />
+      <Composer groupId="community_all" threadId={threadID} />
     </div>
   );
 }
