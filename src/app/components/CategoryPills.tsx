99f066fe377502fd1e621a4a6850ac59e40db746
Maylynn <maylynn@cord.com>
2024-03-18 12:48:54 +0000

    style: category pills to render inline (display: flex)

--- src/app/components/CategoryPills.tsx
+++ src/app/components/CategoryPills.tsx
@@ -1,22 +1,21 @@
 import { NavButton } from '@/app/components/NavButton';
 import { Category } from '@/app/types';
 import { mapCategoryEndpointsToTitles } from '@/utils';
+import styles from './categoryPills.module.css';
 
 export function CategoryPills({ categories }: { categories: Category[] }) {
-  {
-    return (
-      <>
-        {categories &&
-          categories.map((category) => (
-            <NavButton
-              key={category}
-              isActive={false}
-              value={mapCategoryEndpointsToTitles(category)}
-              linkTo={`/category/${category}`}
-              type="category"
-            />
-          ))}
-      </>
-    );
-  }
+  return (
+    <div className={styles.container}>
+      {categories &&
+        categories.map((category) => (
+          <NavButton
+            key={category}
+            isActive={false}
+            value={mapCategoryEndpointsToTitles(category)}
+            linkTo={`/category/${category}`}
+            type="category"
+          />
+        ))}
+    </div>
+  );
 }
