172b3d2309df49ff6363b04608697333f286ac01
Maylynn <maylynn@cord.com>
2024-03-21 13:42:38 +0000

    refactor: sign in / log out links with button component
    This also updates the layout of the header. It doesn't need to be a grid for now so have reverted to a simpler flex box

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,9 +1,9 @@
 import Link from 'next/link';
 import styles from './header.module.css';
-import { SERVER_HOST } from '@/consts';
 import logo from '@/static/cord-logo.png';
 import Image from 'next/image';
 import { getSession } from '@auth0/nextjs-auth0';
+import Button from '../ui/Button';
 
 export default async function Header() {
   const session = await getSession();
@@ -15,23 +15,14 @@ export default async function Header() {
         <Link href="/" aria-label="home">
           <Image alt="cord logo" src={logo} height={40} />
         </Link>
-        {/* These links point to an API route and not to a page, you should keep it as an anchor tag rather than a Link component. */}
         {!user ? (
-          <a
-            href="/api/auth/login"
-            className={styles.signinButton}
-            aria-label="sign in"
-          >
+          <Button type="a" href="/api/auth/login" label="sign in">
             Sign in to comment
-          </a>
+          </Button>
         ) : (
-          <a
-            href="/api/auth/logout"
-            className={styles.signinButton}
-            aria-label="log out"
-          >
+          <Button type="a" href="/api/auth/logout" label="log out">
             Log out {user.name}
-          </a>
+          </Button>
         )}
       </div>
     </header>
