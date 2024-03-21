09e8ac35bbc568f68ac5b20adb982c2e42128bfd
khadija <khadija@cord.com>
2024-03-21 10:22:48 +0000

    more cleanup
    Created using spr 1.3.5

--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -12,22 +12,18 @@ import { NextRequest } from 'next/server';
 export const GET = handleAuth({
   'silent-login': async (req: NextApiRequest, res: NextApiResponse) => {
     const session = await getSession(req, res);
-    const returnTo = cookies().get('return-to')?.value;
-
     if (!session) {
       return handleLogin(req, res, {
-        returnTo: returnTo,
         authorizationParams: {
           prompt: 'none',
         },
       });
     }
   },
-  onError(_: NextRequest, __: HandlerError) {
-    // we can't access the query params, so use a cookie to access the return to address
-    const returnTo = cookies().get('return-to')?.value;
-    cookies().set('retry-login', 'false');
+  onError(req: NextRequest, __: HandlerError) {
+    const returnTo = req.cookies.get('return_to')?.value ?? '/';
+    cookies().set('retry_login', 'false');
 
-    redirect(returnTo ?? '/');
+    redirect(returnTo);
   },
 });
--- src/middleware.ts
+++ src/middleware.ts
@@ -19,7 +19,7 @@ export async function middleware(request: NextRequest) {
   const response = new NextResponse();
   const session = await getSession(request, response);
 
-  const retryLogin = cookies().get('retry-login')?.value;
+  const retryLogin = cookies().get('retry_login')?.value;
   const returnTo = request.nextUrl.pathname;
 
   if (!session?.user && retryLogin !== 'false') {
@@ -29,17 +29,21 @@ export async function middleware(request: NextRequest) {
       `/api/auth/${isCurrentPageNewPost ? 'login' : 'silent-login'}`,
       request.url,
     );
+    // passing the returnTo value as a query param will magically be passed
+    // to auth0, and works out of the box
     loginUrl.searchParams.append('returnTo', returnTo);
-    response.cookies.delete('retry-login');
+    response.cookies.delete('retry_login');
 
     const newResponse = NextResponse.redirect(loginUrl);
-    newResponse.cookies.set('return-to', returnTo);
+    // We have to create this as a cookie, to access it in the handleAuth onError
+    // callback, as that won't have access to the query param set above
+    newResponse.cookies.set('return_to', returnTo);
 
     return newResponse;
   }
 
-  response.cookies.delete('retry-login');
-  response.cookies.delete('return-to');
+  response.cookies.delete('retry_login');
+  response.cookies.delete('return_to');
 
   return NextResponse.next(response);
 }
