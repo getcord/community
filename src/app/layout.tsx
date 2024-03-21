bc7f414193a215087d9dd0d52b1365865d6b5146
dmmiller <dmmiller@cord.com>
2024-03-21 10:37:53 +0000

    Make login actually use the user that you sign in to Auth0 with
    Also clean up our code around categories

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,40 +5,54 @@ import CordIntegration from './CordIntegration';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import styles from './styles.module.css';
-import { Category } from '@/app/types';
-import { SERVER_HOST } from '@/consts';
-import { getSession } from '@auth0/nextjs-auth0';
+import { CATEGORIES } from '@/app/types';
+import { fetchCordRESTApi } from '@/app/fetchCordRESTApi';
+import type { ServerGetUser } from '@cord-sdk/types';
+import { Claims, getSession } from '@auth0/nextjs-auth0';
+
+async function createUserIfNecessary(sessionUser: Claims) {
+  const name = sessionUser.name;
+  const email = sessionUser.email;
+  // Not sure why but sub seems to be the spot that the put the user_id
+  const userID = sessionUser.sub;
+  const profilePictureURL = sessionUser.picture;
+
+  try {
+    await fetchCordRESTApi<ServerGetUser>(`users/${userID}`);
+  } catch (error) {
+    // Let's create the user and add them to the default community group
+    await fetchCordRESTApi(`users/${userID}`, 'PUT', {
+      name,
+      email,
+      profilePictureURL,
+      addGroups: ['community_all'],
+    });
+  }
+  return userID;
+}
 
 async function getData() {
   const { CORD_SECRET, CORD_APP_ID } = process.env;
-  const session = await getSession();
-
+  const categories = [...CATEGORIES];
   if (!CORD_SECRET || !CORD_APP_ID) {
     console.error(
       'Missing CORD_SECRET or CORD_APP_ID env variable. Get it on console.cord.com and add it to .env',
     );
-    return { clientAuthToken: null };
+    return { clientAuthToken: null, categories };
   }
+  const session = await getSession();
+  if (!session) {
+    return { clientAuthToken: null, categories };
+  }
+  const userID = await createUserIfNecessary(session.user);
   const clientAuthToken = getClientAuthToken(CORD_APP_ID, CORD_SECRET, {
-    user_id: 'khadija',
+    user_id: userID,
   });
-  const categoriesResponse = await fetch(`${SERVER_HOST}/api/categories`, {
-    method: 'GET',
-    headers: {
-      'Content-type': 'application/json',
-    },
-  });
-  let categories: Category[];
-  if (categoriesResponse.status !== 200) {
-    categories = [];
-  } else {
-    categories = (await categoriesResponse.json()).data;
-  }
 
   return {
     clientAuthToken,
     categories,
-    user: session?.user,
+    user: session.user,
   };
 }
 
