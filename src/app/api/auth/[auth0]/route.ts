347fe8dabf8ffe5077a149ad6be9f3ffe948e654
khadija <127104309+km-nur@users.noreply.github.com>
2024-03-21 12:14:41 +0000

    Set up SSO && logging in via auth0
    This diff inludes changes to set up login/logout and also makes sure
    single sign on works, but setting up an endpoint to silently login a
    user if they have no session (this is in case we have one from signing
    in via console)
    
    Test Plan: :eyes:
    
    Reviewers: 
    
    Pull Request: https://github.com/getcord/community/pull/2

---
+++ src/app/api/auth/[auth0]/route.ts
@@ -0,0 +1,29 @@
+import {
+  HandlerError,
+  getSession,
+  handleAuth,
+  handleLogin,
+} from '@auth0/nextjs-auth0';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+import { NextRequest } from 'next/server';
+
+export const GET = handleAuth({
+  'silent-login': async (req: NextApiRequest, res: NextApiResponse) => {
+    const session = await getSession(req, res);
+    if (!session) {
+      return handleLogin(req, res, {
+        authorizationParams: {
+          prompt: 'none',
+        },
+      });
+    }
+  },
+  onError(req: NextRequest, __: HandlerError) {
+    const returnTo = req.cookies.get('return_to')?.value ?? '/';
+    cookies().set('attempting_login', 'true');
+
+    redirect(returnTo);
+  },
+});
---
+++ src/middleware.ts
@@ -0,0 +1,49 @@
+import { getSession } from '@auth0/nextjs-auth0/edge';
+import { cookies } from 'next/headers';
+import { NextRequest, NextResponse } from 'next/server';
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (api handlers)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
+};
+
+export async function middleware(request: NextRequest) {
+  const response = new NextResponse();
+  const session = await getSession(request, response);
+
+  const attemptingLogin = cookies().get('attempting_login')?.value;
+  const returnTo = request.nextUrl.pathname;
+
+  if (!session?.user && attemptingLogin !== 'true') {
+    const isCurrentPageNewPost = request.nextUrl.pathname === '/newpost';
+
+    const loginUrl = new URL(
+      `/api/auth/${isCurrentPageNewPost ? 'login' : 'silent-login'}`,
+      request.url,
+    );
+    // passing the returnTo value as a query param will magically be passed
+    // to auth0, and works out of the box
+    loginUrl.searchParams.append('returnTo', returnTo);
+    response.cookies.delete('attempting_login');
+
+    const newResponse = NextResponse.redirect(loginUrl);
+    // We have to create this as a cookie, to access it in the handleAuth onError
+    // callback, as that won't have access to the query param set above
+    newResponse.cookies.set('return_to', returnTo);
+
+    return newResponse;
+  }
+
+  response.cookies.delete('attempting_login');
+  response.cookies.delete('return_to');
+
+  return NextResponse.next(response);
+}
