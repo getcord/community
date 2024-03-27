627e3540bce5ec27b898a43215997e9c8e232c59
dmmiller <dmmiller@cord.com>
2024-03-27 21:11:52 +0000

    Get ThreadID from our KV store
    Use our kv store and increment the nextThreadID value.  This way our posts will increment sequentially and be much more human readable.
    
    Importantly we need to make sure that vercel does not cache fetches within the store.  Added the directive at the top of the page to force it hit the server rather than the cache.

--- src/app/newpost/page.tsx
+++ src/app/newpost/page.tsx
@@ -1,3 +1,8 @@
+// We need to make sure that Vercel will not cache the result of the fetch
+// within kv or else increment will not work properly.  It needs to hit
+// the redis instance each time this is called with no caching
+export const dynamic = 'force-dynamic';
+
 import { kv } from '@vercel/kv';
 
 import Composer from '@/app/components/Composer';
@@ -5,7 +10,8 @@ import { Category } from '@/app/types';
 import { randomUUID } from 'crypto';
 
 async function getData() {
-  return randomUUID();
+  const nextThreadID = await kv.incr('nextThreadID');
+  return `${nextThreadID}`;
 }
 
 export default async function NewPost({
@@ -14,10 +20,9 @@ export default async function NewPost({
   searchParams?: { category: Category };
 }) {
   const threadID = await getData();
-  const n = await kv.incr('nextThreadID');
+
   return (
     <>
-      <p>{n}</p>
       <Composer defaultCategory={searchParams?.category} threadID={threadID} />
     </>
   );
