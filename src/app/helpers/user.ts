aca800d49fbcc2441d6fa2eadce994eab9b5b35b
khadija <khadija@cord.com>
2024-03-27 15:41:57 +0000

    Add is admin field to User Object

--- src/app/helpers/user.ts
+++ src/app/helpers/user.ts
@@ -1,12 +1,14 @@
 import { fetchCordRESTApi } from '@/app/fetchCordRESTApi';
 import type { ServerGetUser } from '@cord-sdk/types';
 import { getSession } from '@auth0/nextjs-auth0';
+import { ADMINS_GROUP_ID } from '@/consts';
 
 export type User = {
   userID?: string;
   name?: string;
   email?: string;
   profilePictureURL?: string;
+  isAdmin?: boolean;
 };
 
 export async function getUser(): Promise<User> {
@@ -20,11 +22,14 @@ export async function getUser(): Promise<User> {
 export async function getUserById(userID: string): Promise<User> {
   try {
     const user = await fetchCordRESTApi<ServerGetUser>(`users/${userID}`);
+    const isAdmin = user.groups.includes(ADMINS_GROUP_ID);
+
     return {
       userID,
       name: user.name ?? undefined,
       email: user.email ?? undefined,
       profilePictureURL: user.profilePictureURL ?? undefined,
+      isAdmin,
     };
   } catch (error) {
     return {};
--- src/consts.ts
+++ src/consts.ts
@@ -6,4 +6,5 @@ export const CORD_CONSOLE_URL = 'https://console.cord.com';
 export const CORD_APP_ID = process.env.CORD_APP_ID!;
 export const CORD_SECRET = process.env.CORD_SECRET!;
 export const EVERYONE_GROUP_ID = 'community_all';
+export const ADMINS_GROUP_ID = 'admins';
 export const SERVER_HOST = process.env.NEXT_PUBLIC_SERVER_HOST!;
