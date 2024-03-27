96e42fa245a679b9b26c82ccc83f177f87c8c40c
dmmiller <dmmiller@cord.com>
2024-03-27 17:13:18 +0000

    Add KV store

--- src/app/newpost/page.tsx
+++ src/app/newpost/page.tsx
@@ -1,18 +1,24 @@
+import { kv } from '@vercel/kv';
+
 import Composer from '@/app/components/Composer';
 import { Category } from '@/app/types';
 import { randomUUID } from 'crypto';
 
-function getData() {
+async function getData() {
   return randomUUID();
 }
 
-export default function NewPost({
+export default async function NewPost({
   searchParams,
 }: {
   searchParams?: { category: Category };
 }) {
-  const threadID = getData();
+  const threadID = await getData();
+  const n = await kv.incr('nextThreadID');
   return (
-    <Composer defaultCategory={searchParams?.category} threadID={threadID} />
+    <>
+      <p>{n}</p>
+      <Composer defaultCategory={searchParams?.category} threadID={threadID} />
+    </>
   );
 }
