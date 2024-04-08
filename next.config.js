528f02f7a248267d6455b5635657d6328f71ca55
Jackson Gabbard <jg@jg.gg>
2024-04-08 16:15:22 +0100

    Fix the next config

--- next.config.js
+++ next.config.js
@@ -7,8 +7,13 @@ const nextConfig = {
         hostname: 'lh3.googleusercontent.com',
         port: '',
       },
+      {
+        protocol: 'https',
+        hostname: 'app.cord.com',
+        port: '',
+      },
     ],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
