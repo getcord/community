c2e0c5a8ee287e148fb8f80cc210ff643bb52779
My Hoa Tien <59407030+mhtien@users.noreply.github.com>
2024-04-09 08:32:49 +0100

    Experiemental - Adding a mark as answer to options menu
    This was me testing whether we could add
    a mark as answer button to the message options menu
    so I could generate some  QandAPage structred data.
    
    Had to change the thread to an experiemental one,
    and for annoyed that I had to store the messageID
    in a state somewhere, and also the threadID
    (thus all the new components with the contexts!)
    
    Also don't show the resolved menu item button when
    you are not an admin.
    
    Hoping Cord 4.0 exposes the threadIDs and MessageIDs
    within the replacement components 🙏 but this currently
    works. When you mark as answer it just updates
    the thread metadata with answerMessageID as the message ID
    that was chosen.
    
    Test Plan:
    Tested locally, then went to console and found
    the thread and saw metadata was updated
    
    Reviewers: dmmiller
    
    Reviewed By: dmmiller
    
    Pull Request: https://github.com/getcord/community/pull/3

--- src/app/post/Post.tsx
+++ src/app/post/Post.tsx
@@ -1,13 +1,26 @@
 'use client';
 
-import { Thread, thread as threadHooks } from '@cord-sdk/react';
+import { thread as threadHooks, experimental } from '@cord-sdk/react';
 import styles from './post.module.css';
 import { getTypedMetadata } from '@/utils';
 import { LockClosedIcon } from '@heroicons/react/24/outline';
 import { PushPinSvg } from '@/app/components/PushPinSVG';
 import { CategoryPills } from '@/app/components/CategoryPills';
 import { Metadata } from '@/app/types';
-import { useEffect } from 'react';
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+} from 'react';
+
+import {
+  MenuItemProps,
+  MenuProps,
+  MessageProps,
+} from '@cord-sdk/react/dist/mjs/types/experimental';
+import { EntityMetadata } from '@cord-sdk/types';
 
 export type ThreadData = {
   id: string;
@@ -15,9 +28,35 @@ export type ThreadData = {
   name: string;
 };
 
-export default function Post({ threadID }: { threadID: string }) {
-  const { thread, loading, hasMore, fetchMore } =
-    threadHooks.useThread(threadID);
+const PostContext = createContext<{
+  userIsAdmin: boolean;
+  threadID: string | null;
+  metadata: EntityMetadata | null;
+}>({
+  userIsAdmin: false,
+  threadID: null,
+  metadata: null,
+});
+
+const MessageContext = createContext<{ messageID: string | null }>({
+  messageID: null,
+});
+
+const REPLACEMENTS = {
+  Message: CommunityMessageWithContext,
+  Menu: CommunityMenu,
+  MenuItem: CommunityMenuItem,
+};
+
+export default function Post({
+  threadID,
+  isAdmin,
+}: {
+  threadID: string;
+  isAdmin: boolean;
+}) {
+  const threadData = threadHooks.useThread(threadID);
+  const { thread, loading, hasMore, fetchMore } = threadData;
 
   useEffect(() => {
     if (hasMore) {
@@ -25,18 +64,24 @@ export default function Post({ threadID }: { threadID: string }) {
     }
   }, [hasMore, fetchMore]);
 
+  const contextValue = useMemo(() => {
+    return {
+      threadID,
+      metadata: thread?.metadata ?? null,
+      userIsAdmin: isAdmin,
+    };
+  }, [isAdmin, thread?.metadata, threadID]);
+
   if (!thread && !loading) {
     return <ThreadNotFound />;
   }
-
   const metadata = getTypedMetadata(thread?.metadata);
 
   return (
-    <>
+    <PostContext.Provider value={contextValue}>
       <ThreadHeading metadata={metadata} threadName={thread?.name || ''} />
-
-      <Thread threadId={threadID} />
-    </>
+      <experimental.Thread thread={threadData} replace={REPLACEMENTS} />
+    </PostContext.Provider>
   );
 }
 
@@ -61,4 +106,83 @@ export function ThreadHeading({
       <CategoryPills categories={metadata.categories} />
     </div>
   );
+}
+
+function CommunityMessageWithContext(props: MessageProps) {
+  const contextValue = useMemo(() => {
+    return { messageID: props.message.id };
+  }, [props.message.id]);
+  return (
+    <MessageContext.Provider value={contextValue}>
+      <experimental.Message {...props} />
+    </MessageContext.Provider>
+  );
+}
+
+function CommunityMenu(props: MenuProps) {
+  const postContext = useContext(PostContext);
+  const messageContext = useContext(MessageContext);
+  const markAsAnswer = useCallback(() => {
+    if (!window.CordSDK) {
+      console.error('Cord SDK not found');
+      return;
+    }
+    if (!postContext.threadID) {
+      console.error('Thread ID not found');
+      return;
+    }
+    if (!messageContext.messageID) {
+      console.error('Message ID not found');
+      return;
+    }
+    window.CordSDK.thread
+      .updateThread(postContext.threadID, {
+        metadata: {
+          ...postContext.metadata,
+          answerMessageID: messageContext.messageID,
+        },
+      })
+      .then(() => {
+        props.closeMenu();
+      })
+      .catch(console.error);
+  }, [
+    messageContext.messageID,
+    props,
+    postContext.metadata,
+    postContext.threadID,
+  ]);
+
+  const markWithAnswer = useMemo(() => {
+    return {
+      name: 'mark-as-answer',
+      element: (
+        <experimental.MenuItem
+          label="Mark as answer"
+          menuItemAction="mark-as-answer"
+          onClick={markAsAnswer}
+        />
+      ),
+    };
+  }, [markAsAnswer]);
+
+  const communityMenuProps = useMemo(() => {
+    if (!postContext.userIsAdmin) {
+      return props;
+    }
+
+    return {
+      ...props,
+      items: [markWithAnswer, ...props.items],
+    };
+  }, [markWithAnswer, postContext.userIsAdmin, props]);
+
+  return <experimental.Menu {...communityMenuProps} />;
+}
+
+function CommunityMenuItem(props: MenuItemProps) {
+  if (props.menuItemAction !== 'thread-resolve') {
+    return <experimental.MenuItem {...props} />;
+  }
+  return null;
 }
