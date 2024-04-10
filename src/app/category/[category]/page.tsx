9a6057ebdd4f9f9c1a360d3185f40b857e86fc68
My Hoa Tien <myhoa@cord.com>
2024-04-10 16:11:34 +0100

    Use mapCategoryEndpointsToTitles to generate titles

--- src/app/category/[category]/page.tsx
+++ src/app/category/[category]/page.tsx
@@ -2,14 +2,14 @@ import { Metadata } from 'next';
 
 import { Category } from '@/app/types';
 import ChatDisplay from './ChatDisplay';
-import { capitalizeFirstLetter } from '@/utils';
+import { mapCategoryEndpointsToTitles } from '@/utils';
 
 type Props = {
   params: { category: Category };
 };
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const category = capitalizeFirstLetter(params?.category);
+  const category = mapCategoryEndpointsToTitles(params?.category);
 
   return {
     title: `${category} | Cord Community`,
