1be667a819822a5d20846125cbf7628bf22c9426
khadija <127104309+km-nur@users.noreply.github.com>
2024-04-19 19:49:11 +0100

    Create new confirmation modal
    Test Plan: used in follow up
    
    Reviewers: dmmiller
    
    Reviewed By: dmmiller
    
    Pull Request: https://github.com/getcord/community/pull/16

---
+++ src/app/components/ConfirmationModal.tsx
@@ -0,0 +1,57 @@
+'use client';
+
+import Divider from '@/app/ui/Divider';
+import Modal, { ModalProps } from './Modal';
+import styles from './confirmationmodal.module.css';
+import Button from '@/app/ui/Button';
+
+type ConfirmationModalProps = ModalProps & {
+  onConfirm: () => void;
+  title: string;
+  confirmActionText: string;
+};
+export default function ConfirmationModal({
+  isOpen,
+  onClose,
+  onConfirm,
+  title,
+  confirmActionText,
+  children,
+}: ConfirmationModalProps) {
+  return (
+    <Modal
+      onClose={onClose}
+      isOpen={isOpen}
+      hasDarkBackground={true}
+      className={styles.modal}
+      onClickOutside={onClose}
+    >
+      <div>
+        <h2 className={styles.title}>{title}</h2>
+        <Divider />
+      </div>
+
+      <div className={styles.content}>
+        {children}
+        <Divider />
+        <div className={styles.footer}>
+          <Button
+            behaveAs={'button'}
+            onClick={onClose}
+            variant={'outline'}
+            aria-label={'Cancel'}
+          >
+            Cancel
+          </Button>
+          <Button
+            behaveAs={'button'}
+            onClick={onConfirm}
+            aria-label={confirmActionText}
+          >
+            {confirmActionText}
+          </Button>
+        </div>
+      </div>
+    </Modal>
+  );
+}
