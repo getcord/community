8b42ae64d42bc04b599adb358a35a030ee890942
Adam Vartanian <flooey@cord.com>
2024-04-24 14:55:06 +0100

    Use UserDataContext
    This lets us provide the user data for the users in a message to
    `MessageContent`, so that it properly renders user mentions when the
    user is logged out.

--- src/app/components/CordClient.tsx
+++ src/app/components/CordClient.tsx
@@ -1,6 +1,18 @@
 'use client';
 import { betaV2 } from '@cord-sdk/react';
+import { UserDataContextType } from '@cord-sdk/react/dist/mjs/types/experimental/hooks/useComponentUserData';
 
 export const CordTimestamp = betaV2.Timestamp;
 export const CordAvatar = betaV2.Avatar;
 export const CordMessageContent = betaV2.MessageContent;
+
+export function CordUserDataProvider({
+  value,
+  children,
+}: React.PropsWithChildren<{ value: UserDataContextType }>) {
+  return (
+    <betaV2.UserDataContext.Provider value={value}>
+      {children}
+    </betaV2.UserDataContext.Provider>
+  );
+}
