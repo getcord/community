79ea63f4285af0105059564e9dca76a6fe619367
khadija <khadija@cord.com>
2024-04-26 16:19:35 +0100

    Clean up support chat admin user logic

--- src/app/support/[customerID]/layout.tsx
+++ src/app/support/[customerID]/layout.tsx
@@ -6,13 +6,19 @@ import { fetchCordRESTApi } from '@/app/fetchCordRESTApi';
 import { ServerGroupData } from '@cord-sdk/types';
 
 async function getData(customerID: string) {
-  const { isAdmin } = await getUser();
+  const { userID, isAdmin } = await getUser();
   if (isAdmin) {
     // For admins, we want to load the customer information and display that
     // rather than the admins customer info (which is presumably Cord)
     const customerGroup = await fetchCordRESTApi<ServerGroupData>(
       `groups/${customerID}`,
     );
+
+    // Make sure they're part of the customer group
+    await fetchCordRESTApi(`groups/${customerID}/members`, 'POST', {
+      add: [userID],
+    });
+
     return {
       customerID,
       customerName: customerGroup?.name,
@@ -21,16 +27,6 @@ async function getData(customerID: string) {
   }
   // Otherwise we can just return the default for the customer
   return getCustomerInfo();
-}
-
-async function addAdminIfNecessary(customerID: string) {
-  const user = await getUser();
-  if (user.isAdmin) {
-    // Now lets make sure the admin is part of the group
-    await fetchCordRESTApi(`groups/${customerID}/members`, 'POST', {
-      add: [user.userID],
-    });
-  }
 }
 
 export default async function Layout({
@@ -49,8 +45,6 @@ export default async function Layout({
     // if they have account but not support enabled show upgrade button
     return <h1>Oh no! Looks like you don&apos;t have a Cord account</h1>;
   }
-
-  await addAdminIfNecessary(params.customerID);
 
   return (
     <div className={styles.container}>
