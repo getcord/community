e5520e85ec39582cd5a9e652c48c1cd434dbc89e
dmmiller <dmmiller@cord.com>
2024-04-26 13:43:57 +0100

    Fix errors about ForwardRef and keys in React

--- src/app/support/[customerID]/[threadID]/page.tsx
+++ src/app/support/[customerID]/[threadID]/page.tsx
@@ -5,10 +5,63 @@ import { XMarkIcon } from '@heroicons/react/24/solid';
 import styles from './threadDetails.module.css';
 import { useRouter } from 'next/navigation';
 import { PaginationTrigger } from '@/app/components/PaginationTrigger';
+import { Fragment, forwardRef } from 'react';
+
+const CommunityThreadLayout = forwardRef(function CommunityThreadLayout(
+  props: betaV2.ThreadLayoutProps,
+  ref: React.ForwardedRef<HTMLElement>,
+) {
+  const threadData = props.threadData;
+  if (!threadData) {
+    return <betaV2.ThreadLayout ref={ref} {...props} />;
+  }
+
+  const { loading, fetchMore, hasMore, thread } = threadData;
+  const messagesWithReplyCount = threadData.messages.map((message) => {
+    if (
+      message.id !== thread?.firstMessage?.id ||
+      loading === undefined ||
+      fetchMore === undefined ||
+      hasMore === undefined
+    ) {
+      return <betaV2.Message key={message.id} message={message} />;
+    }
+
+    const numReplies = (thread?.total ?? 1) - 1;
+    const repliesText = `${numReplies} ${
+      numReplies === 1 ? 'reply' : 'replies'
+    }`;
+    return (
+      <Fragment key={'fragment_key'}>
+        <betaV2.Message message={message} />
+        {numReplies > 0 && (
+          <div className={styles.divider}>
+            <span>{repliesText}</span>
+            <hr className={styles.line} />
+          </div>
+        )}
+        <PaginationTrigger
+          loading={loading}
+          hasMore={hasMore}
+          fetchMore={fetchMore}
+        />
+      </Fragment>
+    );
+  });
+
+  return (
+    <betaV2.ThreadLayout
+      ref={ref}
+      {...props}
+      messages={messagesWithReplyCount}
+    />
+  );
+});
 
 const REPLACEMENTS: betaV2.ReplaceConfig = {
   ThreadLayout: CommunityThreadLayout,
 };
+
 export default function ThreadDetails({
   params,
 }: {
@@ -35,47 +88,4 @@ export default function ThreadDetails({
       <betaV2.Thread threadData={threadData} replace={REPLACEMENTS} />
     </div>
   );
-}
-
-function CommunityThreadLayout(props: betaV2.ThreadLayoutProps) {
-  const threadData = props.threadData;
-  if (!threadData) {
-    return <betaV2.ThreadLayout {...props} />;
-  }
-
-  const messagesWithReplyCount = threadData.messages.map((message) => {
-    const { loading, fetchMore, hasMore, thread } = threadData;
-
-    if (
-      message.id !== thread?.firstMessage?.id ||
-      loading === undefined ||
-      fetchMore === undefined ||
-      hasMore === undefined
-    ) {
-      return <betaV2.Message key={message.id} message={message} />;
-    }
-
-    const numReplies = (thread?.total ?? 1) - 1;
-    const repliesText = `${numReplies} ${
-      numReplies === 1 ? 'reply' : 'replies'
-    }`;
-    return (
-      <>
-        <betaV2.Message key={message.id} message={message} />
-        {numReplies > 0 && (
-          <div className={styles.divider}>
-            <span>{repliesText}</span>
-            <hr className={styles.line} />
-          </div>
-        )}
-        <PaginationTrigger
-          loading={loading}
-          hasMore={hasMore}
-          fetchMore={fetchMore}
-        />
-      </>
-    );
-  });
-
-  return <betaV2.ThreadLayout {...props} messages={messagesWithReplyCount} />;
 }
