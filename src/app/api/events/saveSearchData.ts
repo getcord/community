cbdfcdce9069f4854096a86514cacf4b6c5d63a7
khadija <khadija@cord.com>
2024-05-23 11:00:04 +0100

    fix errors and rebase
    Created using spr 1.3.5

--- src/app/api/events/saveSearchData.ts
+++ src/app/api/events/saveSearchData.ts
@@ -47,36 +47,40 @@ export async function saveSearchData(
   }
 
   if (messages.length > 0) {
-    // if it's a reply delete the existing entry in db so we can regenerate
-    // the vector embedding with new message.
-    if (!isFirstMessage) {
-      await deleteURLFromIndex(COMMUNITY_SEARCH_INDEX, thread.url);
-    }
+    try {
+      // if it's a reply delete the existing entry in db so we can regenerate
+      // the vector embedding with new message.
+      if (!isFirstMessage) {
+        await deleteURLFromIndex(COMMUNITY_SEARCH_INDEX, thread.url);
+      }
 
-    // Make sure to include categories, post title and authors as searchable elements.
-    const categories = `Categories: ${getCategoriesString(thread.metadata)}`;
-    const title = `Title: ${thread.name}`;
-    const conversationArray = [categories, title];
+      // Make sure to include categories, post title and authors as searchable elements.
+      const categories = `Categories: ${getCategoriesString(thread.metadata)}`;
+      const title = `Title: ${thread.name}`;
+      const conversationArray = [categories, title];
 
-    // breaks down the cord message object to readable plaintext string
-    // that we can easily convert to a vector. Make sure to not have weird
-    // characters or symbols to make it clear to the llm what's searchable.
-    messages.forEach((message) => {
-      conversationArray.push(
-        `${message.authorDisplayName}: ${message.plaintext.replace(
-          /\n/g,
-          ' ',
-        )}`,
+      // breaks down the cord message object to readable plaintext string
+      // that we can easily convert to a vector. Make sure to not have weird
+      // characters or symbols to make it clear to the llm what's searchable.
+      messages.forEach((message) => {
+        conversationArray.push(
+          `${message.authorDisplayName}: ${message.plaintext.replace(
+            /\n/g,
+            ' ',
+          )}`,
+        );
+      });
+      const chunk = conversationArray.join('\n');
+      const embedding = await createEmbedding(chunk);
+      await storeChunk(
+        COMMUNITY_SEARCH_INDEX,
+        chunk,
+        embedding,
+        thread.name,
+        thread.url,
       );
-    });
-    const chunk = conversationArray.join('\n');
-    const embedding = createEmbedding(chunk);
-    await storeChunk(
-      COMMUNITY_SEARCH_INDEX,
-      chunk,
-      embedding,
-      thread.name,
-      thread.url,
-    );
+    } catch (_) {
+      // do nothing
+    }
   }
 }
--- src/lib/search/api.ts
+++ src/lib/search/api.ts
@@ -5,43 +5,32 @@ export async function storeChunk(
   title: string,
   url: string,
 ) {
-  try {
-    await fetch(`${process.env.SEARCH_DATA_API_HOST}/api/chatContext`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        secret: process.env.COMMUNITY_APP_SECRET,
-        index,
-        chunk,
-        embedding,
-        title,
-        url,
-      }),
-    });
-  } catch (e) {
-    console.log(`Failed to store chunk: ${JSON.stringify({ index, url })}`, e);
-  }
+  await fetch(`${process.env.SEARCH_DATA_API_HOST}/api/storeChunk`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      secret: process.env.SEARCH_DATA_API_SECRET,
+      index,
+      chunk,
+      embedding,
+      title,
+      url,
+    }),
+  });
 }
 
 export async function deleteURLFromIndex(index: string, url: string) {
-  try {
-    await fetch(`${process.env.SEARCH_DATA_API_HOST}/api/chatContext`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        secret: process.env.SEARCH_DATA_API_SECRET,
-        index,
-        url,
-      }),
-    });
-  } catch (e) {
-    console.error(
-      `Failed to delete url from index: ${JSON.stringify({ index, url })}`,
-      e,
-    );
-  }
+  await fetch(`${process.env.SEARCH_DATA_API_HOST}/api/chatContext`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      secret: process.env.SEARCH_DATA_API_SECRET,
+      index,
+      url,
+    }),
+  });
 }
