a1dcf2a1fdf96fe7b31e0db81bf253022edff964
khadija <127104309+km-nur@users.noreply.github.com>
2024-05-29 17:02:51 +0100

    Add full page search to handle mobile cases
    Test Plan:
    https://github.com/getcord/community/assets/127104309/125c94b7-2c25-4d25-9148-79b7fc0b7c67
    
    Reviewers:
    
    Pull Request: https://github.com/getcord/community/pull/40

--- src/app/components/ThreadList.tsx
+++ src/app/components/ThreadList.tsx
@@ -88,6 +88,54 @@ export function ThreadListHeader({
 }) {
   return (
     <section className={styles.header}>
+      <div
+        style={{
+          padding: 16,
+          background: '#F6F1FF',
+          borderRadius: 16,
+          color: '#333',
+          marginBottom: 32,
+        }}
+      >
+        <div style={{ maxWidth: 600, margin: '0 auto' }}>
+          <p>
+            <strong>
+              Cord's hosted service (both free and paid versions), and the
+              company, is shutting down in August.
+            </strong>
+          </p>
+          <p>
+            However, Cord's code is becoming open source, and you can freely use
+            it in your own infrastructure.
+          </p>
+          <p>
+            Shutting down is sad for us - Cord's employees, the clients who
+            supported us along the journey, and investors. But we are very happy
+            to be supported by our team and our investors in the decision to
+            make Cord free for everyone to use. We have worked hard for years to
+            make it easy for any developer to add real-time commenting and
+            collaboration in their app, and we've built a solution used by tens
+            of thousands of people across products and industries - from video
+            players to digital whiteboards, collaborative drawing applications
+            to finance and BI tools, and many more. We are deeply thankful to
+            our customers for their support, feedback, and belief in us. We are
+            incredibly proud of what we've accomplished, and incredibly grateful
+            to our investors for their trust, support, advice - and for pushing
+            for the right ending, where Cord is available for everyone to use.
+          </p>
+          <p>
+            If you're using Cord now, or want to get collaboration into your
+            app, our Open Source repository (more details coming shortly)
+            contains all the details about how to install the backend service,
+            and the entire code for the SDK that you can host, package and ship
+            with your application.
+          </p>
+          <p>
+            We hope that Cord will continue to benefit people, develop and grow
+            in your applications. We can't wait to see what you build next.
+          </p>
+        </div>
+      </div>
       <h1 className={styles.pageTitle}>
         {category ? mapCategoryEndpointsToTitles(category) : 'All Posts'}
       </h1>
